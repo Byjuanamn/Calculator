9c73f0291ad9b4603829845c636ea5ea66cef7ca
byjuanamn <juan.antonio.martin@gmail.com>
2017-04-27 16:02:48 +0200

    solution baseline
    Proyecto de ejemplo con Xamarin Forms

---
+++ Calculator/CalculatorHome.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+	public partial class CalculatorHome : ContentPage
+	{
+		public CalculatorHome()
+		{
+			InitializeComponent();
+		}
+
+		async void OnAddOperation(object sender, EventArgs e)
+		{
+			var operando1 = Convert.ToInt32(numberText1.Text);
+			var operando2 = Convert.ToInt32(numberText2.Text);
+
+			var result = operando1 + operando2;
+
+			resultLbl.Text = Convert.ToString(result);
+			System.Diagnostics.Debug.WriteLine(String.Format("El resultado es --> {0}", result));
+
+			await Task.WhenAny<bool>(
+			 resultLbl.ScaleTo(2, 250)
+			);
+
+			await resultLbl.ScaleTo(1, 500);
+
+
+		}
+
+		void OnProductOperation(object sender, EventArgs e)
+		{
+			// metemos un displayAlert
+			this.DisplayAlert("Aviso", "Esta funcionalidad no está implemetada aún", "Ok");
+		}
+
+		void OnMinusOperation(object sender, EventArgs e)
+		{
+			var operando1 = Convert.ToInt32(numberText1.Text);
+			var operando2 = Convert.ToInt32(numberText2.Text);
+
+			var result = operando1 * operando2;
+
+			resultLbl.Text = Convert.ToString(result);
+			System.Diagnostics.Debug.WriteLine(String.Format("El resultado es --> {0}", result));
+
+		}
+	}
+}
